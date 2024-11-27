4351ea98e357d7c3067392c946c8ef27874ae336
ericks97 <erickaljndrr@gmail.com>
2024-11-27 14:09:57 -0300

    comentarios

--- frontend/js/logout.js
+++ frontend/js/logout.js
@@ -1,7 +1,5 @@
-// logout.js
 document.addEventListener('DOMContentLoaded', function() {
-    const logoutButton = document.querySelector('.button-53'); // O el selector correcto de tu botón de logout
-    
+    const logoutButton = document.querySelector('.button-53');
     // Verifica si el botón existe en el DOM antes de intentar agregar el eventListener
     if (logoutButton) {
       logoutButton.addEventListener('click', logout);
@@ -9,12 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
       console.warn('El botón de cerrar sesión no se encontró en el DOM.');
     }
   });
-  
   function logout() {
-    // Eliminar el estado de sesión
-    sessionStorage.removeItem('userLoggedIn'); // o sessionStorage.setItem('userLoggedIn', false);
-    
-    // Redirigir al login
+    sessionStorage.removeItem('userLoggedIn');
     window.location.href = 'login.html';
   }
   
