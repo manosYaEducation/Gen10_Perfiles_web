4b423eedfda10cd66fbd1715e05c75d4af39e623
ericks97 <erickaljndrr@gmail.com>
2024-11-28 16:39:58 -0300

    entorno local/production

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,10 +1,23 @@
 <?php
 
-$host = 'localhost';
-$port = '3306';
-$user = 'root';
-$password = '';
-$nameDb = 'alphadoc_kreative';
+define('environment', 'local'); // NOTA IMPORTANTE: SE DEBE CAMBIAR LA VARIABLE "environment" local a production cuando el proyecto esté en producción.
+
+
+if(environment === 'production') {  
+    $host = 'localhost';
+    $port = '3306';
+    $user = 'alphadoc';
+    $password = 'No.Olvidemos.Que.Un.Trigger.Es.Un.diparador.22';
+    $nameDb = 'alphadoc_kreative';
+
+}else{
+    $host = 'localhost';
+    $port = '3306';
+    $user = 'root';
+    $password = '';
+    $nameDb = 'alphadoc_kreative';
+}
+
 
 $dsn = "mysql:host=$host;port=$port;dbname=$nameDb;user=$user;password=$password";
 
