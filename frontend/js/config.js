fbed4f90f36ced5abe159bdbe793f44195a4fd9f
Iparedes <ivanparedes98@hotmail.es>
2025-01-23 17:34:54 -0300

    Arreglo de rutas

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,13 +1,13 @@
 window.API_URL = (window.location.hostname === 'localhost' || 
     window.location.hostname === '127.0.0.1')
 ? 'http://localhost:8000/'
-: 'https://krative.alphadocere.cl';
+: 'https://kreative.alphadocere.cl';
 
 
 
 window.API_URL_PHP = (window.location.hostname === 'localhost' || 
     window.location.hostname === '127.0.0.1')
 ? 'http://localhost/Gen10_Perfiles_web/backend/'
-: 'https://krative.alphadocere.cl/backend/';
+: 'https://kreative.alphadocere.cl/backend/';
 
 
