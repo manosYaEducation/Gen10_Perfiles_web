13d946da2f725d1619fa127c332a5d3862012c10
Mcornejo084g <m.cornejo084.g@gmail.com>
2025-04-07 10:36:50 -0400

    Cambiometadata.js

---
+++ frontend/js/metadata.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    if (!window.API_URL_PHP) {
+        console.error('API_URL_PHP no está definida');
+        return;
+    }
+    try {
+        const response = await fetch(`${window.API_URL_PHP}read_user.php?id=${id}`);
+        const result = await response.json();
+        const profile = result.data;
+
+        // Mostrar imagen de perfil si existe
+        if (result.data.image) {
+            // METADATA
+            // CambiarNombre
+            var metadatanombre = document.querySelector('meta[name="author"]');
+            const nombreperfil = profile.basic.name;
+            metadatanombre.setAttribute("content", nombreperfil);
+            // console.log(metadatanombre.getAttribute("content")); --Confirmar Cambio en Consola            
+        }
+    } catch (error) {
+        console.error('Error al obtener los datos:', error);
+    }
+});
