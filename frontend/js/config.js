84d9ef13f496bbc5a5dcbf6f72a62cf553143a5c
Ivan <ivanparedes98@hotmail.es>
2025-04-14 11:58:29 -0400

    arreglo config

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,18 +1,11 @@
-// Detectar si estamos en entorno local
-const isLocalEnvironment =
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1";
+window.API_URL = (window.location.hostname === 'localhost' || 
+  window.location.hostname === '127.0.0.1')
+? 'http://localhost/Gen10_Perfiles_web/'
+: 'https://kreative.alphadocere.cl';
 
-// Configurar la URL de la API según el entorno
-// Solo necesitamos una asignación, no dos
-window.API_URL_PHP = isLocalEnvironment
-  ? "http://localhost:3000/backend/"
-  : "https://kreative.alphadocere.cl/backend/";
 
-// Si necesitas una URL alternativa para XAMPP, puedes usar otra variable
-// window.API_URL_XAMPP = isLocalEnvironment
-//   ? "http://localhost/Gen10_Perfiles_web/backend/"
-//   : "https://kreative.alphadocere.cl/backend/";
 
-console.log("Entorno:", isLocalEnvironment ? "local" : "producción");
-console.log("API URL:", window.API_URL_PHP);
+window.API_URL_PHP = (window.location.hostname === 'localhost' || 
+  window.location.hostname === '127.0.0.1')
+? 'http://localhost/Gen10_Perfiles_web/backend/'
+: 'https://kreative.alphadocere.cl/backend/';
