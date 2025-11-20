656d777117734ac347e85bf6027482ca7dcdc97a
Cristian <diazcristian0210@gmail.com>
2025-11-20 17:31:11 -0300

    Consistencias en el tipado

--- frontend/js/read_client.js
+++ frontend/js/read_client.js
@@ -92,7 +92,7 @@ document.addEventListener("DOMContentLoaded", async function () {
                             <h3 class="profile-subtitle">${client.company || ''}</h3>
                             <a href="../frontend/client-template.html?id=${client.id}" class="button-link">Perfil</a>
                             <button class="buttonActualizar" data-id="${client.id}" onclick="redirectToUpdateClient(${client.id})">Actualizar</button>
-                            <button class="buttonBorrar" data-id="${client.id}" onclick="deleteClient(event)">Borrar</button>
+                            <button class="buttonBorrar" data-id="${client.id}" onclick="deleteClient(event)">Eliminar</button>
                         </div>
                     `;
                     clientsProfilesColumn.appendChild(clientCard);
